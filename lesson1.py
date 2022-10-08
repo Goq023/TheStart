c2c61fa92b07ffcdadcf69ed2f1ee9689da04723
Goq023 <111569290+Goq023@users.noreply.github.com>
2022-10-08 18:36:40 +0400

    add file

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+print ("Hello World!")
+print ("We learning Python")
+print ("open")
+print ("next")
+print ("close")
+# We learned print command
+
+#Homework
+
+
+print ("*")
+print ("**")
+print ("***")
+print ("**")
+print ("*")
+
+#HW2
+message = ('Tolstoy wrote "Anna Karenina" in 1877')
+print (message)
---
+++ lesson2.py
@@ -0,0 +1,203 @@
+# # # print("inefqpsdfq")
+# # #
+# # # a = 10
+# # # b = 535
+# # # c = 13
+# # #
+# # # print(a)
+# #
+# #
+# # # # integer
+# # # var1 = 146
+# # # var2 = 213541
+# # #
+# # # # float
+# # # var3 = 1.5
+# # # var4 = 52.6
+# # #
+# # # # string
+# # # var5 = "New String"
+# # # var6 = "hEllo Number 4 & 52"
+# # #
+# # # # boolean
+# # # var7 = True
+# # # var8 = False
+# # #
+# # # print(var5)
+# # #
+# # # print(type(var7))
+# #
+# #
+# # # # TypeCast / Type Conversion
+# # # a = 10
+# # # b = a
+# # #
+# # # print(b)
+# # # print(type(b))
+# #
+# #
+# #
+# # # c = "Hello"
+# # # print(c)
+# # # c = "World"
+# # # print(c)
+# #
+# # # ctrl + ?
+# #
+# # # a = 10
+# # # b = str(a)
+# # #
+# # # print(b)
+# # # print(type(b))
+# # #
+# # # print(a)
+# # # print(type(a))
+# #
+# # # c = "100"
+# # # d = int(c)
+# # #
+# # # print(d)
+# # #
+# # # e = float(c)
+# # # print(e)
+# # #
+# # #
+# # # f = 100.8
+# # #
+# # #
+# # # print(int(f))
+# #
+# # # h = "100.9"
+# # # j = int(h)
+# # #
+# # # print(j)
+# #
+# # # a = int(float("10.8"))
+# # # print(a)
+# #
+# #
+# #
+# # # a = 150
+# # # b = "Hello"
+# # # c = 3.14
+# # # d = 640
+# # # e = a + d
+# # # print(e)
+# # # print(a + e)
+# # # # print(a + b)
+# # # f = "101"
+# # # # g = a + int(f)
+# # # print(a + int(f))
+# #
+# #
+# # # str1 = "Narek's Laptop"
+# # # str2 = 'Lev Tolstoy wrote Anna Karenina in "1887" '
+# # # str3 = "Lev Tolstoy wrote Anna Karenina in \"1887\" "
+# #
+# # # str1 = "Hello"
+# # # str2 = "World"
+# # # str3 = " "
+# # # print(str1 + str3 + str2 + "!")
+# #
+# #
+# # # print(10 + 20)
+# # # print(10 + 2.3)
+# # # print(10 + int(2.3))
+# # # print(10 - 3)
+# # # print(3 * 43)
+# # # print(10 / 3)  # 3.3333333333333335
+# # # print(10 // 3)
+# # # print(10 % 3)
+# # # print(2 ** 3)
+# # #
+# # # print("A" + "B")
+# # # print("A" * 3)
+# #
+# #
+# #
+# # # Տպել A տառը այնքան անգամ ինչքան 6 կա 129-ում։
+# #
+# # # a = 129/6
+# # # print("A" * int(a))
+# #
+# # # count = 129//6
+# # # print("A" * count)
+# #
+# #
+# #
+# #
+# # # int1 = 10
+# # # int1 = int1 + 5
+# # # print(int1)
+# # # print(int1)
+# #
+# # # int2 = 10
+# # # int2 += 5  # int2 = int2 + 5
+# #
+# #
+# #
+# # # b1 = True
+# # # b2 = False
+# # #
+# # # print(b1 and b2)
+# # # print(b1 or b2)
+# # # print((b1 and b2) or b2)
+# # #
+# # # print(not b1)
+# #
+# #
+# #
+# # # a = 10 / 5
+# # # print(a)
+# #
+# #
+# # print(10 > 5)
+# # a = 100
+# # b = 31
+# #
+# # print(a//b > 2)
+# # print(a//b == 3)
+# #
+# # print(a//b != 8)
+# #
+# # print(10 == 10.0)
+# # print(10 == 10.31)
+# # print(10 == int(10.31))
+# #
+# #
+# # a = 103
+# # b = 104
+# # res = a > b
+# # print(res)
+# # print(type(res))
+# #
+# #
+#
+#
+# # Homework 1
+#
+# a = 125 // 11
+# print("Hello" * a)
+#
+# Homework 2
+# str1 = input ("How many\n")
+# print ("There are" + (str1) +"ducks in the lake")
+#
+# #Homework 3
+#
+# num1 = 23
+# num2 = 43
+# num3 = 3
+# num4 = 54
+# print(num1 > 10 and num2 > 10 and num3 > 10 and num4 > 10)
+
+
+# Homework 4
+
+# user_data = (int(input("Number 1\n")))
+# if user_data > 10:
+#     print("Numer is bigger than 10")
+
+
+txt = " fdk jjsakd aas kdsa jkas jdsakd"
+print (len(txt))
